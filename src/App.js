6fe1fe37f03a4956ceb1e90bed2eea821bf6ae73
Karina Martinez <kmart67@berkeley.edu>
2022-05-11 15:24:12 -0700

    Progress on react

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+ BrowserRouter,
+ Route,
+ Switch,
+} from "react-router-dom";
+
+import HomePage from "./components/HomePage"
+import PhotoPage from "./components/PhotoPage"
+import ContactPage from "./components/ContactPage"
+import Header from "./components/Header"
+import { Paths } from "./models/Routes"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Switch>
+        <Route exact path={Paths.Photos}>
+          <PhotoPage/>
+        </Route>
+        <Route exact path={Paths.Contact}>
+          <ContactPage/>
+        </Route>
+        <Route exact path={Paths.Home}>
+          <HomePage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/models/Videos.js
@@ -0,0 +1,9 @@
+export const Videos = [
+  {"name": "Maxo Kream-Local Joker", "id": "582278789"},
+  {"name": "reggie-“I Don’t Want to Feel No More”", "id": "582277084"},
+  {"name": "424-SS22 Milan Fashion Week", "id": "582277817"},
+  {"name": "reggie-Southside Fade", "id": "582278382"},
+  {"name": "reggie-Traffic", "id": "686978456"},
+  {"name": "Tanna Leone-pgLang", "id": "687026097"},
+  {"name": "Maxo Kream Ft. Tyler The Creator- Big Persona", "id": "602225659"},
+];
